e6f45863653a287511435b87beb42328f693712e
Okereke Princewill <vincentprincewill44@gmail.com>
2022-06-27 20:35:16 +0000

    Task

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+strr = strr[39:75] + " " + strr[:6]
+print(str)
