a7d0da4fb4ab059c010d1b538dd66ff96fa6c20f
Okereke Princewill <101342119+CrimsonKarma44@users.noreply.github.com>
2022-07-06 22:34:09 +0100

    Task

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,8 +3,8 @@ def uniq_add(my_list=[]):
     listd = []
     count = 0
     for i in my_list:
-       if (i in listd):
-          continue
-       listd.append(i)
-       count += i
+        if (i in listd):
+            continue
+        listd.append(i)
+        count += i
     return (count)
