5156bf733c7d24dd5c5cacde87798810ceed7da0
Okereke Princewill <101342119+CrimsonKarma44@users.noreply.github.com>
2022-07-19 22:47:51 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
 
 class Square:
-    """define a square"""
+    """define a square."""
+
     def __init__(self, size):
-        """size is a private instance attribute"""
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
